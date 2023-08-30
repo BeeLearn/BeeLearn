2bac01779ae41ddfdf4e630eb941a9f23ae66393
Oguntunde Caleb <usegong@gmail.com>
2023-08-31 00:27:59 +0100

    Switch whole project Getx - new deployment

--- lib/views/fragments/topic_fragment.dart
+++ lib/views/fragments/topic_fragment.dart
@@ -1,5 +1,4 @@
 import 'package:beelearn/services/ad_loader.dart';
-import 'package:beelearn/views/components/animation_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
